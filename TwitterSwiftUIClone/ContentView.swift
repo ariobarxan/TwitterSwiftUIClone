70aa7598a0134a75e01c78128904b3f97debd76a
Azhman <bapir.dev@gmail.com>
2022-09-05 08:49:19 +0430

    Initial Commit

---
+++ TwitterSwiftUIClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwitterSwiftUIClone
+//
+//  Created by Amirreza Zarepour on 9/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwitterSwiftUIClone/TwitterSwiftUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUICloneApp.swift
+//  TwitterSwiftUIClone
+//
+//  Created by Amirreza Zarepour on 9/5/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
