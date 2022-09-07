27acb6edf4ae3637af8839845a6d0348598d5ed2
Azhman <bapir.dev@gmail.com>
2022-09-07 12:03:17 +0430

    Editting ContentView

--- TwitterSwiftUIClone/View/ContentView.swift
+++ TwitterSwiftUIClone/View/ContentView.swift
@@ -1,16 +1,26 @@
 //
 //  ContentView.swift
-//  TwitterSwiftUIClone
+//  TwitterClone
 //
-//  Created by Amirreza Zarepour on 9/5/22.
+//  Created by Amirreza Zarepour on 1/1/21.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    //MARK: - Var
+    @EnvironmentObject private var viewModel: SignInViewModel
+    
+    //MARK: - MainBody
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Group{
+            if viewModel.userSeession != nil{
+                MainTabView()
+                    
+            }else{
+                SigninView()
+            }
+        }
     }
 }
 
