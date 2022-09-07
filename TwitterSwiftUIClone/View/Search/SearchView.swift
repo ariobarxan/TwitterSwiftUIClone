099a84af4cc21f45800ffee12c21effb8bcd18e0
Azhman <bapir.dev@gmail.com>
2022-09-07 11:59:43 +0430

    SerachView is added

---
+++ TwitterSwiftUIClone/View/Search/SearchView.swift
@@ -0,0 +1,57 @@
+//
+//  SearchView.swift
+//  TwitterClone
+//
+//  Created by Amirreza Zarepour on 8/24/22.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    
+    //MARK: - Var
+    @StateObject private var viewModel = SearchViewModel()
+    
+    //MARK: - MainBody
+    var body: some View {
+        ScrollView{
+                
+            SearchBar(searchString: $viewModel.searchText)
+                .padding()
+                
+            VStack{
+                ForEach(viewModel.searchText.isEmpty ? viewModel.users : viewModel.filteredUsers()){ user in
+                      
+                    HStack{
+                        NavigationLink(destination:
+                            LazyView(
+                                UserProfileView()
+                                .environmentObject(UserProfileViewModel(user: user))
+                            )
+                        ){
+                            UserCell(user: user)
+                        }
+                        
+                            
+                        Spacer()
+                    }
+                    .padding(.bottom, 5)
+                }
+            }
+            .padding(.leading)
+        }
+        .onAppear{
+            viewModel.updateUsers()
+        }
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView{
+            SearchView()
+                .navigationTitle("Search")
+                .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
