2f6448bdddb9aadd09f48fd345b1991c504c5fe9
Azhman <bapir.dev@gmail.com>
2022-09-07 12:04:10 +0430

    App File is editted

--- TwitterSwiftUIClone/TwitterSwiftUICloneApp.swift
+++ TwitterSwiftUIClone/TwitterSwiftUICloneApp.swift
@@ -1,17 +1,39 @@
 //
-//  TwitterSwiftUICloneApp.swift
-//  TwitterSwiftUIClone
+//  TwitterCloneApp.swift
+//  TwitterClone
 //
-//  Created by Amirreza Zarepour on 9/5/22.
+//  Created by Amirreza Zarepour on 8/23/22.
 //
 
 import SwiftUI
+import Firebase
 
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//  func application(_ application: UIApplication,
+//                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//    FirebaseApp.configure()
+//
+//    return true
+//  }
+//}
 @main
-struct TwitterSwiftUICloneApp: App {
+struct TwitterCloneApp: App {
+    // register app delegate for Firebase setup
+    //  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView{
+               ContentView()
+                    .environmentObject(SignInViewModel())
+            }
+            
         }
     }
 }
+
+//Bugfix navigationview title should be explixitly noted based on the tab in the main tab view
